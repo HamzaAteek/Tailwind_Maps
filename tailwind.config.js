6a0fed8dadc7ce08fe6a45fc35e632767afa8f47
Hamza Ateek <hamzaateek@gmail.com>
2024-11-20 11:47:26 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "#F5F7FA",
-        primary: "#4D4D4D",
-        secondary: "#71717",
-        button: "#4CAF4F",
-        title: "#263238",
+        primary: "#458FF6",
+        secondary: "#7D7987",
       },
     },
   },
